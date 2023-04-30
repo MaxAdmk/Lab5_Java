9883457162cff02f3433c36ac1a46966f9180549
MaxAdmk <maksimkeny@gmail.com>
2023-04-30 22:00:42 +0300

    adding 5 lab code

--- src/main/java/ua/lviv/iot/sentencesEditor/Main.java
+++ src/main/java/ua/lviv/iot/sentencesEditor/Main.java
@@ -2,16 +2,27 @@ package ua.lviv.iot.sentencesEditor;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public final class Main {
 
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter text for redacting: ");
-    String text = input.nextLine();
+    private Main() {
 
-    Editor sentencesEditor = new Editor();
-    System.out.println("Updated text: ");
-    System.out.println(sentencesEditor.removeExclamatorySentencesWithDashWords(text));
-    input.close();
+    }
+
+    /**
+     * The {@code Main} class is the entry point of the program.
+     * It allows the user to input a text and uses
+     * a {@link TextEditor}
+     * object to remove exclamatory sentences with dash words from the text.
+     */
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter text for redacting: ");
+        String text = input.nextLine();
+
+        TextEditor sentencesTextEditor = new TextEditor();
+        System.out.println("Updated text: ");
+        sentencesTextEditor.
+                removeExclamatorySentencesWithDashWords(text);
+        input.close();
     }
 }
